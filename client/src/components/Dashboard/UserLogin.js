a52ba5d9dbe5b0ba1b9b2bf48a040ed9b48766b5
Pooria <p.asiabi@gmail.com>
2022-10-10 16:43:06 +0330

    fix errors

--- client/src/components/Dashboard/UserLogin.js
+++ client/src/components/Dashboard/UserLogin.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const UserLogin = ({user}) => {
-
+const UserLogin = ({ user }) => {
   console.log(user);
   return <div>{user.name}</div>;
 };
 
-
-const mapStateToProps=state=>({
-    user:state.auth.user
-})
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+});
 
 export default connect(mapStateToProps)(UserLogin);
-
